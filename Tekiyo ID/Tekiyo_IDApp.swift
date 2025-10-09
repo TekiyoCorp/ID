a596e86517ab22a65ebaebb5aed3f6d215806b08
TekiyoCorp <190143017+TekiyoCorp@users.noreply.github.com>
2025-10-10 00:55:27 +0800

    Initial Commit

---
+++ Tekiyo ID/Tekiyo_IDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tekiyo_IDApp.swift
+//  Tekiyo ID
+//
+//  Created by zak on 10/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Tekiyo_IDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
